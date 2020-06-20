f85578e3f953d0e9db9549bda34154916a3937db
Getty Ritter <gettylefou@gmail.com>
2020-06-19 17:56:06 -0700

    A number of API expansions and some configurability

--- Data/Digest/DrunkenBishop.hs
+++ Data/Digest/DrunkenBishop.hs
@@ -1,6 +1,13 @@
 {-# LANGUAGE BinaryLiterals #-}
 
-module Data.Digest.DrunkenBishop (drunkenBishop) where
+module Data.Digest.DrunkenBishop
+  ( -- * Drunken Bishop
+    drunkenBishop,
+    -- * Configurable Drunken Bishop
+    DrunkenBishopOptions(..),
+    drunkenBishopDefaultOptions,
+    drunkenBishopWithOptions
+  ) where
 
 import Data.Array
 import Data.Bits
@@ -9,15 +16,12 @@ import qualified Data.ByteString.Lazy as BSL
 import Data.Digest.Pure.MD5
 import Data.Word (Word8)
 
-type Board = Array (Int, Int) Int
+type Board = Array (Int, Int) Word8
 
-initialPosition :: (Int, Int)
-initialPosition = (8, 4)
-
-mkBoard :: Board
-mkBoard = array bound [(i, 0) | i <- range bound]
+mkBoard :: (Int, Int) -> Board
+mkBoard (width, height) = array bound [(i, 0) | i <- range bound]
   where
-    bound = ((0, 0), (16, 8))
+    bound = ((0, 0), (width -1, height -1))
 
 toDirections :: BS.ByteString -> [Dir]
 toDirections bs = case BS.uncons bs of
@@ -40,14 +44,14 @@ toDir x = go (x .&. 0b11)
     go 0b11 = DR
     go _ = error "unreachable"
 
-move :: Dir -> (Int, Int) -> (Int, Int)
-move d (a, b) = snap (go d (a, b))
+move :: (Int, Int) -> Dir -> (Int, Int) -> (Int, Int)
+move (width, height) d (a, b) = snap (go d (a, b))
   where
-    go UL (x, y) = (x -1, y -1)
-    go UR (x, y) = (x + 1, y -1)
-    go DL (x, y) = (x -1, y + 1)
+    go UL (x, y) = (x - 1, y - 1)
+    go UR (x, y) = (x + 1, y - 1)
+    go DL (x, y) = (x - 1, y + 1)
     go DR (x, y) = (x + 1, y + 1)
-    snap (x, y) = (clamp x 0 16, clamp y 0 8)
+    snap (x, y) = (clamp x 0 (width - 1), clamp y 0 (height - 1))
 
 clamp :: Ord a => a -> a -> a -> a
 clamp n low high
@@ -55,8 +59,12 @@ clamp n low high
   | n > high = high
   | otherwise = n
 
-toChar :: Int -> Char
-toChar n = case n of
+-- | The default mapping from byte to ASCII character used by
+-- OpenSSH. The intention of this mapping was to have characters of
+-- increasing "noise" as the byte value goes up, with 15 and 16
+-- instead mapping to special "begin" and "end" characters.
+drunkenBishopOpenSSHCharMap :: Word8 -> Char
+drunkenBishopOpenSSHCharMap n = case n of
   00 -> ' '
   01 -> '.'
   02 -> 'o'
@@ -76,20 +84,75 @@ toChar n = case n of
   16 -> 'E'
   _ -> '?'
 
-runSteps :: (Int, Int) -> [Dir] -> Board -> Board
-runSteps pos [] b = b // [(pos, 16)]
-runSteps pos (d : ds) b =
-  let newPos = move d pos
-   in if b ! pos == 15
-        then runSteps newPos ds b
-        else runSteps newPos ds (b // [(newPos, clamp ((b ! newPos) + 1) 0 14)])
-
-drunkenBishop :: BSL.ByteString -> String
-drunkenBishop bs = render (runSteps initialPosition (toDirections h) mkBoard // [((8, 4), 15)])
+-- | Once we've converted a hash into a sequence of two-bit 'Dir'
+-- values, we can walk it, incrementing the values of board locations
+-- as we pass them
+runSteps :: (Int, Int) -> (Int, Int) -> [Dir] -> Board -> Board
+runSteps _ pos [] b = b // [(pos, 16)]
+runSteps size pos (d : ds) b
+  | b ! pos == 15 = runSteps size newPos ds b
+  | otherwise = runSteps size newPos ds (b // [(newPos, clamp ((b ! newPos) + 1) 0 14)])
   where
-    render b =
-      unlines
-        [ foldr (:) "" [toChar (b ! (x, y)) | x <- [0 .. 16]]
-          | y <- [0 .. 8]
-        ]
-    h = md5DigestBytes (md5 bs)
+    newPos = move size d pos
+
+-- | Convert a finalized board into a string
+renderBoard :: DrunkenBishopOptions input -> Board -> String
+renderBoard opts board =
+  unlines
+    [ foldr (:) "" [drunkenBishopCharMap opts (board ! (x, y)) | x <- [0 .. width -1]]
+      | y <- [0 .. height -1]
+    ]
+  where
+    (width, height) = drunkenBishopBoardSize opts
+
+-- | A set of configuration options for specializing the Drunken
+-- Bishop algorithm.
+data DrunkenBishopOptions input = DrunkenBishopOptions
+  { drunkenBishopHash :: input -> BS.ByteString,
+    -- ^ The hashing function to use in order to convert an input
+    -- value into a hash usable for a Drunken Bishop run.
+    drunkenBishopBoardSize :: (Int, Int),
+    -- ^ The board size used for a Drunken Bishop run.
+    drunkenBishopInitialPosition :: Maybe (Int, Int),
+    -- ^ The initial position of the bishop on the board. If this is
+    -- 'Nothing', then the initial position will be @(width `div` 2,
+    -- height `div` 2)@.
+    drunkenBishopCharMap :: Word8 -> Char
+    -- ^ The mapping from bytes to characters used for visualizing a
+    -- Drunken Bishop run. The values @0x0@ through @0xE@ correspond
+    -- to how many times the bishop has visited the cell in the course
+    -- of a walk, while the values @0xE@ and @0xF@ are 'special' in
+    -- that they represent the starting and ending position of the
+    -- bishop, respectively.
+  }
+
+-- | The options used by the OpenSSH implementation of Drunken
+-- Bishop. This uses the MD5 hash algorithm, starting the bishop at
+-- the center of a a 17x9 grid, and uses [the character table
+-- described here](http://www.dirk-loss.de/sshvis/drunken_bishop.pdf).
+drunkenBishopDefaultOptions :: DrunkenBishopOptions BSL.ByteString
+drunkenBishopDefaultOptions =
+  DrunkenBishopOptions
+    { drunkenBishopHash = md5DigestBytes . md5,
+      drunkenBishopBoardSize = (17, 9),
+      drunkenBishopInitialPosition = Nothing,
+      drunkenBishopCharMap = drunkenBishopOpenSSHCharMap
+    }
+
+-- | Run the Drunken Bishop algorithm with the options chosen by
+-- OpenSSH. See the documentation on 'drunkenBishopDefaultOptions' for
+-- specifics on what those are.
+drunkenBishop :: BSL.ByteString -> String
+drunkenBishop = drunkenBishopWithOptions drunkenBishopDefaultOptions
+
+-- | Run the Drunken Bishop algorithm with the provided set of options.
+drunkenBishopWithOptions :: DrunkenBishopOptions input -> input -> String
+drunkenBishopWithOptions opts bs = renderBoard opts finalBoard
+  where
+    (width, height) = drunkenBishopBoardSize opts
+    initialBoard = mkBoard (width, height) // [(initialPosition, 15)]
+    bishopPath = toDirections (drunkenBishopHash opts bs)
+    finalBoard = runSteps (width, height) initialPosition bishopPath initialBoard
+    initialPosition = case drunkenBishopInitialPosition opts of
+      Nothing -> (width `div` 2, height `div` 2)
+      Just pos -> pos
